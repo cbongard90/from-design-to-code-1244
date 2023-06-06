11ac140769d71fe2c7c748bb721920da36b1566e
Christian Bongard <christian.bongard90@gmail.com>
2023-06-06 11:57:47 +0400

    added the movie update

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -8,8 +8,14 @@ export default class extends Controller {
     'list'
   ]
   connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget);
-    console.log(this.listTarget);
+  }
+
+  update() {
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data
+      })
   }
 }
